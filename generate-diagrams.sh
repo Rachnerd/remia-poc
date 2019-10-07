e95e368a41cd1977594b5858fa1b1d370542eb06
Rachnerd <rachel@openvalue.nl>
2019-10-07 22:32:42 +0200

    Added diagram for Angular component hierarchy.

--- generate-diagrams.sh
+++ generate-diagrams.sh
@@ -1,2 +1,3 @@
 puml generate diagrams/api/request-flow.puml -o diagrams/api/request-flow.png
 puml generate diagrams/api/request-flow-REST.puml -o diagrams/api/request-flow-REST.png
+puml generate diagrams/remia-poc/components.puml -o diagrams/remia-poc/components.png
